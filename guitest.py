86f59a4dee75efc041fb477402756a238573e161
Sam Ralston <samralston@sam-ralston.localdomain>
2020-01-14 00:03:12 +1000

    Cleaned up, + feedback

--- guitest.py
+++ guitest.py
@@ -48,22 +48,23 @@ def the_gui():
         event, values = window.Read(timeout=100)       # wait for up to 100 ms for a GUI event
         if event == "Submit":
             try:
-                
-
                 threading.Thread(target=guitaskgiver, args=(gui_queue,values,), daemon=True).start()
-            
-            
+                # Tell user its printing... hopefully 
+                window.Element('pro').Update("Printing your daysheets...")
             except Exception as e:
                 print(e)
+                # tell the user whats up 
+                window.Element('pro').Update(e)
         # --------------- Check for incoming messages from threads  ---------------
         try:
-            message = gui_queue.get_nowait()
+            message =  gui_queue.get_nowait()
         except queue.Empty:             # get_nowait() will get exception when Queue is empty
             message = None              # break from the loop if no more messages are queued up
 
         # if message received from queue, display the message in the Window
         if message:
-            print('Got a message back from the thread: ', message)
+        # messages back from the thread from qu
+            window.Element('pro').Update(message)
 
     # if user exits the window, then close the window and exit the GUI func
     window.Close()
@@ -73,8 +74,8 @@ def the_gui():
     
 def guitaskgiver(gui_queue,values):
     maker = daysheetmaker(values['startdate'],values['enddate'],values['printername'])
-    maker.runner()
-    gui_queue.put('** Done **')  # put a message into queue for GUI
+    maker.runner(gui_queue)
+    
       
     
 
